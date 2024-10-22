d39a7b3641e8ca3b2f3f509e2ca77f43ff3b044e
tcguus <gugucram15@gmail.com>
2024-10-22 08:59:44 -0300

    Criando o Header.

--- cp3/src/app/components/Header/Header.tsx
+++ cp3/src/app/components/Header/Header.tsx
@@ -1,7 +1,12 @@
 export default function Header () {
     return(
-        <header>
-            <a href="" className="">Home</a>
+        <header className="w-full h-[4rem] bg-gray-500 text-white font-bold flex items-center">
+            <a href="" className="ml-[1rem] text-lg transform hover:scale-110 transition duration-300">Home</a>
+            <ul className="flex ml-auto mr-[1rem] gap-7 text-lg">
+                <li className="transform hover:scale-110 transition duration-300"><a href="">CheckPoints</a></li>
+                <li className="transform hover:scale-110 transition duration-300"><a href="">Global Solution</a></li>
+                <li className="transform hover:scale-110 transition duration-300"><a href="">Challenger Sprints</a></li>
+            </ul>
         </header>
     );
 }
--- cp3/src/app/layout.tsx
+++ cp3/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./components/Header/Header";
 
 export const metadata: Metadata = {
   title: "CP03",
@@ -13,9 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body
-        
-      >
+      <head>
+      <link rel="icon" href="/logo-fiap.ico/" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"/>
+      </head>
+      <body>
+        <Header />
         {children}
       </body>
     </html>
