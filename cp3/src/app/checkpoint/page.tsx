2d2b56e48f457ff90ab55597f1e0b3d0c8abdf9e
rodrig1509 <digosmantovanello@gmail.com>
2024-10-27 20:48:58 -0300

    Criando página checkpoint.

--- cp3/src/app/checkpoint/page.tsx
+++ cp3/src/app/checkpoint/page.tsx
@@ -1,8 +1,30 @@
+import Image from "next/image";
 
-export default function Checkpoint () {
-    return (
-        <>
-        <main>check</main>
-        </>
-    );
+export default function Checkpoint() {
+  return (
+    <>
+      <main className="flex flex-col justify-center items-center h-[calc(100vh-4rem)] bg-gray-800 text-white">
+        <div className="flex flex-col justify-center w-full">
+          <p className="ml-[0.5rem]">
+            Confira a seguir o ultimo CheckPoint realizado por Gustavo Camargo,
+            Rodrigo Sousa e Leonardo Cesar:
+          </p>
+        </div>
+        <Image
+          src="/checkpoint-1.png"
+          alt="Checkpoint 1"
+          width={600}
+          height={300}
+          className="mt-[1.5rem]"
+        />
+        <Image
+          src="/checkpoint-2.png"
+          alt="Checkpoint 1"
+          width={600}
+          height={300}
+          className="mt-[1.5rem]"
+        />
+      </main>
+    </>
+  );
 }
