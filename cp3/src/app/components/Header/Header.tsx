115dc35358e56b8e1dc5d13cc3b55afc5bdc399f
tcguus <gugucram15@gmail.com>
2024-10-27 23:28:42 -0300

    Ajustes finais, finalizado.

--- cp3/src/app/components/Header/Header.tsx
+++ cp3/src/app/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header className="w-full h-[4rem] bg-gray-500 text-white font-bold flex items-center">
-            <a href="/" className="ml-[1rem] text-lg transform hover:scale-110 transition duration-300">Home</a>
+            <Link href="/" className="ml-[1rem] text-lg transform hover:scale-110 transition duration-300">Home</Link>
             <ul className="flex ml-auto mr-[1rem] gap-7 text-lg">
                 <li className="transform hover:scale-110 transition duration-300"><Link href={"/checkpoint"}>CheckPoint</Link></li>
                 <li className="transform hover:scale-110 transition duration-300"><Link href={"/challenge"}>Challenge Sprint</Link></li>
